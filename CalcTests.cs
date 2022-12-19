f9d16f57e70023f98c4eab817c5049b8ffdd7f38
laocane <adamboryslawski97@gmail.com>
2022-12-19 18:18:04 +0100

    initial commit

---
+++ CalcTests.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class CalcTests
+{
+    [TestMethod]
+    public void ShouldAdd_GivenTwoInts()
+    {
+        var a = 1;
+        var b = 1;
+        var result = Calc.Add(a, b);
+        Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void ShouldSubstract_GivenTwoInts()
+    {
+        var a = 1;
+        var b = 1;
+        var result = Calc.Substract(a, b);
+        Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void ShouldMultiply_GivenTwoInts()
+    {
+        var a = 1;
+        var b = 1;
+        var result = Calc.Multiply(a, b);
+        Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void ShouldDivide_GivenTwoInts()
+    {
+        var a = 1;
+        var b = 1;
+        var result = Calc.Divide(a, b);
+        Assert.AreEqual(1, result);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(Calc.Add(1, 3));
+
+public class Calc
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
